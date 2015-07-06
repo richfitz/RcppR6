c81f2efb40479b954360a0927eaffff751fc5179
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-07-06 12:45:18 +1000

    Update to work with most recent R6
    (plus fix incorrect check for testthat failure)

--- inst/examples/examples/tests/testthat/test-simple.R
+++ inst/examples/examples/tests/testthat/test-simple.R
@@ -3,8 +3,7 @@ context("Simple Classes")
 test_that("empty", {
   e <- empty()
   expect_that(e, is_a("empty"))
-  expect_that(ls(e), is_identical_to("initialize"))
-  expect_that(ls(e, all.names=TRUE),
-              is_identical_to(c(".ptr", "initialize")))
+  expect_that(sort(ls(e)),
+              is_identical_to(sort(c("clone", "initialize"))))
   expect_that(e$.ptr, is_a("externalptr"))
 })
--- tests/testthat/helper-RcppR6.R
+++ tests/testthat/helper-RcppR6.R
@@ -1,7 +1,7 @@
 passes_tests <- function() {
   function(package) {
     res <- devtools::test(package)
-    expectation(!any(as.data.frame(res)$error),
+    expectation(sum(as.data.frame(res)$failed) == 0,
                 "tests failed", "tests passed")
   }
 }
